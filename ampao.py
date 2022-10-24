48d6af146b9e90401e3e2da523f168a80d61ea30
gamagamol <gamaariefsadya@gmail.com>
2022-10-24 16:14:55 +0700

    soal telah di kerjakan

--- ampao.py
+++ ampao.py
@@ -1,4 +1,25 @@
-u = 16
-k = 3
-ampao = u**(1/2)
-print(ampao)
+import psycopg2 as ps
+import pandas as pd
+
+param={
+    "host":"localhost",
+    "database":"postgres",
+    "user":"postgres",
+    "password":"postgres",
+    "port":5432
+}
+
+conn=ps.connect(**param)
+cursor=conn.cursor()
+
+pelukis_squery='SELECT * FROM pelukis'
+cursor.execute(pelukis_squery)
+pelukis=cursor.fetchall()
+
+cursor.close()
+conn.close()
+
+df=pd.DataFrame(pelukis)
+df.columns=['id_pelukis','nama_pelukis']
+
+df.to_csv(r'C:\Users\N I T R O\OneDrive\Documents\Data Engineering\CS-Fundamental-DS\Gama_Ariefsadya.csv', index=False)
